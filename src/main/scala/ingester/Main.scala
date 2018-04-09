51136e2a9f108fd2ef1d7555ff6a4e0fb3fd83e3
Francesco <brunifrancesco02@gmail.com>
2018-04-09 08:05:17 +0200

    Provide some params from command line

--- src/main/scala/ingester/Main.scala
+++ src/main/scala/ingester/Main.scala
@@ -16,20 +16,25 @@ import org.apache.spark.rdd._
 import org.apache.spark.storage.StorageLevel
 
 object Main extends App {
+  val inputDir = args(0)
+  val outputCatalog = args(1)
+  val layerName = args(2)
+  val noDataValue = args(3)
+
   val conf =
     new SparkConf()
       .setMaster("local[1]")
+      .setAppName("Spark Tiler")
       .set("spark.executor.instances", "1")
       .set("spark.executor.cores", "1")
-      .setAppName("Spark Tiler")
       .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
       .set("spark.kryo.registrator", "geotrellis.spark.io.kryo.KryoRegistrator")
 
   implicit val sc = new SparkContext(conf)
   try {
-    println("Starting...")
+
     val inputRdd: RDD[(ProjectedExtent, MultibandTile)] =
-      sc.hadoopMultibandGeoTiffRDD("/Users/boss/pk/shapefiles/retiled")
+      sc.hadoopMultibandGeoTiffRDD(inputDir).mapValues(_.withNoData(Some(noDataValue.toDouble)))
 
     println(s"Partitions: ${inputRdd.partitions.length}")
 
@@ -51,15 +56,14 @@ object Main extends App {
     reprojected.persist(StorageLevel.MEMORY_AND_DISK_SER)
 
     // Create the attributes store that will tell us information about our catalog.
-    val attributeStore = FileAttributeStore("/tmp/catalog")
+    val attributeStore = FileAttributeStore(outputCatalog)
 
     // Create the writer that we will use to store the tiles in the local catalog.
     val writer = FileLayerWriter(attributeStore)
 
     // Pyramiding up the zoom levels, write our tiles out to the local file system.
     Pyramid.upLevels(reprojected, layoutScheme, 10, Bilinear) { (rdd, z) =>
-      println(s"Processing ${z}")
-      val layerId = LayerId("spinua", z)
+      val layerId = LayerId(layerName, z)
       // If the layer exists already, delete it out before writing
       if(attributeStore.layerExists(layerId)) {
         new FileLayerManager(attributeStore).delete(layerId)
